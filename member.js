9edc9ed42a32fa80ac126a81b3b836dd5f7ae094
limzzzim <qnhuhbt@gmail.com>
2023-01-15 18:35:42 +0700

    complete

--- member.js
+++ member.js
@@ -1,65 +1,231 @@
 class Info {
-    constructor(name, dob, phone, email, onboard, team) {
+    constructor(id, name, dob, phone, email, onboard, teamCode) {
+        this.id = id;
         this.name = name;
         this.dob = dob;
         this.phone = phone;
         this.email = email;
         this.onboard = onboard;
-        this.team = team;
+        this.teamCode = teamCode;
     }
 }
 
-let infor = [];
+let infors = [];
 
 const info_data = "info_data";
 function init() {
     if (localStorage.getItem(info_data) == null) {
-        infor = [
-            new Info('Phương Thảo', '21/10/1993', '0934500914', 'pthao@gmail.com', '01/09/2020', 'Front End'),
-            new Info('Thanh Tiến', '06/07/1992', '0344130980', 'ttien@gmail.com', '01/09/2020', 'Unity'),
-            new Info('Văn Tài', '31/01/1994', '0914783868', 'vtai@gmail.com', '01/07/2021', 'Unity'),
-            new Info('Quỳnh Như', '12/06/2000', '0975584814', 'qnhu@gmail.com', '19/04/2022', 'Office'),
-            new Info('Anh Tuấn', '20/06/1995', '0376657371', 'atuan@gmail.com', '07/02/2022', 'Back End'),
-            new Info('Thanh Xuân', '13/03/1990', '0907192975', 'txuan@gmail.com', '19/04/2022', 'Quality Asurance'),
-            new Info('Đức Biên', '09/01/1995', '0869181745', 'dbien@gmail.com', '25/04/2022', 'Front End'),
-            new Info('Thúy Vy', '29/05/1994', '0979071150', 'tvy@gmail.com', '22/08/2022', 'Bridge Engineer'),
-            new Info('Daiki Machida', '16/05/1997', '0344691403', 'daiki@gmail.com', '01/09/2020', 'Office'),
-            new Info('Quốc Bảo', '06/04/1995', '0902725724', 'qbao@gmail.com', '04/05/2022', 'Back End'),
+        infors = [
+            new Info(1, 'Phương Thảo', '1993-12-20', '0934500914', 'pthao@gmail.com', '1993-12-20', 'fe'),
+            new Info(2, 'Thanh Tiến', '1993-12-20', '0344130980', 'ttien@gmail.com', '1993-12-20', 'unity'),
+            new Info(3, 'Văn Tài', '1993-12-20', '0914783868', 'vtai@gmail.com', '1993-12-20', 'unity'),
+            new Info(4, 'Quỳnh Như', '1993-12-20', '0975584814', 'qnhu@gmail.com', '1993-12-20', 'office'),
+            new Info(5, 'Anh Tuấn', '1993-12-20', '0376657371', 'atuan@gmail.com', '1993-12-20', 'be'),
+            new Info(6, 'Thanh Xuân', '1993-12-20', '0907192975', 'txuan@gmail.com', '1993-12-20', 'qa'),
+            new Info(7, 'Đức Biên', '1993-12-20', '0869181745', 'dbien@gmail.com', '1993-12-20', 'fe'),
+            new Info(8, 'Thúy Vy', '1993-12-20', '0979071150', 'tvy@gmail.com', '1993-12-20', 'brse'),
+            new Info(9, 'Daiki Machida', '1993-12-20', '0344691403', 'daiki@gmail.com', '1993-12-20', 'office'),
+            new Info(10, 'Quốc Bảo', '1993-12-20', '0902725724', 'qbao@gmail.com', '1993-12-20', 'be'),
         ]
-        localStorage.setItem(info_data, JSON.stringify(infor));
+        localStorage.setItem(info_data, JSON.stringify(infors));
     } else {
-        infor = JSON.parse(localStorage.getItem(info_data));
+        infors = JSON.parse(localStorage.getItem(info_data));
     }
 }
+
+let teams = [
+    {
+        code: "be",
+        name: "Back End"
+    },
+    {
+        code: "fe",
+        name: "Front End"
+    },
+    {
+        code: "qa",
+        name: "Quality Assurance"
+    },
+    {
+        code: "brse",
+        name: "Bridge Engineer"
+    },
+    {
+        code: "unity",
+        name: "Unity"
+    },
+    {
+        code: "office",
+        name: "Office"
+    }
+]
+
+let inforIdUpdate;
 
 function renderInfor() {
     let tbInfor = document.getElementById('tbInfor');
     tbInfor.innerHTML = "";
-    for (let i = 0; i < infor.length; i++) {
+    for (let i = 0; i < infors.length; i++) {
+        let team = findTeamByCode(infors[i].teamCode);
         tbInfor.innerHTML += `
         <tr>
             <td>${i + 1}</td>
-            <td>${infor[i].name}</td>
-            <td>${infor[i].dob}</td>
-            <td>${infor[i].phone}</td>
-            <td>${infor[i].email}</td>
-            <td>${infor[i].onboard}</td>
-            <td>${infor[i].team}</td>
+            <td>${infors[i].name}</td>
+            <td>${infors[i].dob}</td>
+            <td>${infors[i].phone}</td>
+            <td>${infors[i].email}</td>
+            <td>${infors[i].onboard}</td>
+            <td>${team.name}</td>
+            <td><button class="edit" onclick="handleEditClick(${infors[i].id})">edit</button> / <button class="edit" onclick="handleDeleteClick(${infors[i].id})">delete</button></td>
         </tr>
         `
     }
 }
 
-function addInfor() {
+function addInfo() {
     let name = document.getElementById('name').value;
     let dob = document.getElementById('dob').value;
     let phone = document.getElementById('phone').value;
     let email = document.getElementById('email').value;
     let onboard = document.getElementById('onboard').value;
-    let team = document.getElementById('team').value;
+    let code = document.getElementById('team').value;
 
-    let newInfo = new Info(name, dob, phone, email, onboard, team);
-    addInfor.push(newInfo);
+    if (name == "" || dob == "" || phone == "" || email == "" || onboard == "" || code == "") {
+        alert('You need to provide information');
+    } else {
+        let maxIdInfo = findMaxIdInfo();
+        let id;
+        if (maxIdInfo == null) {
+            id = 1;
+        } else {
+            id = maxIdInfo.id + 1;
+        }
+        let newInfo = new Info(id, name, dob, phone, email, onboard, code);
+        infors.push(newInfo);
+        localStorage.setItem(info_data, JSON.stringify(infors));
+        renderInfor();
+    }
+    // constructor(id, name, dob, phone, email, onboard, team) {
 }
 
+function findTeamByCode(code) {
+    for (let i = 0; i < teams.length; i++) {
+        if (teams[i].code == code) {
+            return teams[i];
+        }
+    }
+    return null;
+}
+
+function findMaxIdInfo() {
+    if (infors.length == 0) {
+        return null;
+    } else {
+        let max = infors[0].id;
+        let indexMax = 0; // vi tri có id lớn nhất
+        for (let i = 1; i < infors.length; i++) {
+            if (infors[i].id > max) {
+                max = infors[i].id;
+                indexMax = i;
+            }
+        }
+        return infors[indexMax];
+    }
+}
+function findInfoById(inforId) {
+    for (let i = 0; i < infors.length; i++) {
+        if (infors[i].id == inforId) {
+            return infors[i];
+        }
+    }
+    return null;
+}
+
+function handleEditClick(inforId) {
+
+    inforIdUpdate = inforId;
+    let info = findInfoById(inforId);
+    console.log(info);
+    document.getElementById('name').value = info.name;
+    document.getElementById('dob').value = info.dob;
+    document.getElementById('phone').value = info.phone;
+    document.getElementById('email').value = info.email;
+    document.getElementById('onboard').value = info.onboard;
+    document.getElementById('team').value = info.teamCode;
+
+    document.getElementById("btnAdd").style.display = "none";
+    document.getElementById("btnUpdate").style.display = "block";
+    document.getElementById("btnCancel").style.display = "block";
+    localStorage.setItem(info_data, JSON.stringify(infors));
+    renderInfor();
+}
+
+function handleDeleteClick(inforId) {
+    let check = confirm('Are you sure?' + inforId);
+    if (check == true) {
+        let indexInfo = findIndexInforById(inforId);
+        infors.splice(indexInfo, 1);
+        localStorage.setItem(info_data, JSON.stringify(infors));
+        renderInfor();
+    }
+}
+function findIndexInforById(inforId) {
+    for (let i = 0; i < infors.length; i++) {
+        if (infors[i].id == inforId) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function updateInfo() {
+    let name = document.getElementById('name').value;
+    let dob = document.getElementById('dob').value;
+    let phone = document.getElementById('phone').value;
+    let email = document.getElementById('email').value;
+    let onboard = document.getElementById('onboard').value;
+    let code = document.getElementById('team').value;
+    if (name == "" || dob == "" || phone == "" || email == "" || onboard == "" || code == "") {
+        alert('You need to provide information');
+    } else {
+        let i = findIndexInforById(inforIdUpdate);
+
+        infors[i].name = name;
+        infors[i].dob = dob;
+        infors[i].phone = phone;
+        infors[i].email = email;
+        infors[i].onboard = onboard;
+        infors[i].teamCode = code;
+
+        localStorage.setItem(info_data, JSON.stringify(infors));
+        renderInfor();
+
+        document.getElementById('name').value = "";
+        document.getElementById('dob').value = "";
+        document.getElementById('phone').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('onboard').value = "";
+        document.getElementById('team').value = "";
+        document.getElementById("btnAdd").style.display = "block";
+        document.getElementById("btnUpdate").style.display = "none";
+        document.getElementById("btnCancel").style.display = "none";
+    }
+}
+
+function cancelInfo() {
+    let check = confirm('Are you sure?');
+    if (check == true) {
+        document.getElementById('name').value = "";
+        document.getElementById('dob').value = "";
+        document.getElementById('phone').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('onboard').value = "";
+        document.getElementById('team').value = "";
+        document.getElementById("btnAdd").style.display = "block";
+        document.getElementById("btnUpdate").style.display = "none";
+        document.getElementById("btnCancel").style.display = "none";
+    }
+}
+
+init();
 renderInfor();
